3739af27fe7f36e4192754a29878a473d33467e3
Fatih Eser <afeser@users.noreply.github.com>
2020-10-07 16:26:37 +0300

    integer arithmatic bug fix

--- check.js
+++ check.js
@@ -1,35 +1,35 @@
 var total = 0;
 
 var check = function(){
-  var alinacak_ders_kodu = '3124137'; // course code or * for every course
+  var alinacak_ders_kodu = '3121101'; // course code or * for every course
   var ders_kodu_index = 1;
-  var num_coluns = 8;
+  var num_columns = 8;
   var capacity_index = 6;
   var used_capacity_index = 7;
-  var kontrol_araligi = 40;
 
 
   console.log('Deneme', total);
   var i;
-  for (i = 0; i < document.getElementsByClassName('clickable').length; i++) {
-    if(i % 8 == 6){
+  for (i = 0; i < document.getElementsByClassName('clickable').length / num_columns; i++) {
 
-      if(document.getElementsByClassName('clickable')[capacity_index].textContent != document.getElementsByClassName('clickable')[used_capacity_index].textContent &&
-        (document.getElementsByClassName('clickable')[ders_kodu_index].textContent == alinacak_ders_kodu || document.getElementsByClassName('clickable')[ders_kodu_index].textContent == "*")){
 
-        console.log('Ders BULUNDU!')
+    if(document.getElementsByClassName('clickable')[capacity_index+i*num_columns].textContent != document.getElementsByClassName('clickable')[used_capacity_index+i*num_columns].textContent &&
+      (document.getElementsByClassName('clickable')[ders_kodu_index+i*num_columns].textContent == alinacak_ders_kodu || document.getElementsByClassName('clickable')[ders_kodu_index+i*num_columns].textContent == "*")){
 
-        var a = new Audio("https://raw.githubusercontent.com/afeser/METU-Course-Capacity-Check/main/aaa.mp3");
-        a.play();
-      }
+      console.log(document.getElementsByClassName('clickable')[capacity_index+i*num_columns].textContent);
+      console.log(document.getElementsByClassName('clickable')[used_capacity_index+i*num_columns].textContent);
+
+      console.log('Ders BULUNDU!')
+
+      var a = new Audio("hhttps://raw.githubusercontent.com/afeser/METU-Course-Capacity-Check/main/aaa.mp3");
+      a.play();
     }
   }
 
   document.getElementById('submitSearchForm').click();
   total = total + 1;
 
-  setTimeout(check, kontrol_araligi*1000); // wait X seconds(loading of page takes X seconds for me...), to refresh page... TODO : dynamic decision would be great!
+  setTimeout(check, 40000); // wait 25 seconds(takes 25 seconds for me...), to refresh page... TODO : dynamic decision would be great!
 }
 
 check();
-
